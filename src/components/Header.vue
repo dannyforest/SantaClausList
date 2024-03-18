b82f8758656694cf9f3713a567d774494c265ac0
TanyaCanse <tanya_canse@live.ca>
2024-03-18 13:38:54 -0400

    First header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,64 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <nav
+    class="navbar navbar-expand-md navbar-light sticky-top"
+    style="background-color: #e3f2fd"
+  >
+    <div class="container-fluid">
+      <router-link to="/" class="navbar-brand">
+        <img
+          src="..\assets\3885d324-9ba3-4dbd-bde3-7d81cd0d0322.webp"
+          alt="Logo"
+          class="rounded"
+          style="height: 100px"
+        />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
+          <li class="nav-item">
+            <div class="nav-link">Home</div>
+          </li>
+          <li class="nav-item">
+            <div class="nav-link">About Us</div>
+          </li>
+        </ul>
+        <div class="text-center">
+          <button class="btn btn-outline-info m-1 rounded">FR</button>
+          <button class="btn btn-outline-info m-1 rounded">EN</button>
+        </div>
+        <div class="text-center">
+          <button class="btn m-1 rounded">
+            <i class="bi bi-box-arrow-right" style="font-size: 25px"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<style scoped>
-
+<style>
+.navbar {
+  background-image: linear-gradient(
+    to right,
+    green,
+    white,
+    red,
+    green,
+    white,
+    red,
+    green,
+    white,
+    red
+  );
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './assets/main.css'
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
