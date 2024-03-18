6694613fc6117b05423cbedf50add3682ae7ecd7
Danny Forest <ford86@gmail.com>
2024-03-18 12:47:11 -0400

    First Commit

---
+++ src/components/SantaClausList.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Santa Claus List</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AddToList from "@/components/AddToList.vue";
+import Header from "@/components/Header.vue";
+import SantaClausList from "@/components/SantaClausList.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <main>
+    <Header />
+    <SantaClausList />
+    <AddToList />
+    <Footer />
+  </main>
+</template>
