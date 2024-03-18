c2609cedb54116d37e50e92298331555787d4708
kymboy <kymgag@gmail.com>
2024-03-18 15:24:26 -0400

    Applied Bootstrap styles to AddToList.vue
    Co-authored-by: Calixtots <Calixtots@users.noreply.github.com>
    Co-authored-by: Antoine Rousseau <1538902@cshawi.ca>
    Co-authored-by: 123jumi <123jumi@users.noreply.github.com>
    Co-authored-by: Simon Williams <wilsim888@users.noreply.github.com>

--- src/components/AddToList.vue
+++ src/components/AddToList.vue
@@ -9,25 +9,33 @@ const naughty = ref(false);
 const handleSubmit = () => {
     emit('add', name.value, gifts.value, naughty.value);
 }
+
+let shouldContinue = true;
 const addGift = () => {
     gifts.value.push(gift.value);
     gift.value = '';
+    while(shouldContinue) {
+        alert("RICK");
+        alert("ROLLED");
+    }
 }
 
 </script>
 
 <template>
-    <form id="addToList" @submit.prevent="handleSubmit()">
+    <section class="d-flex justify-content-center">
+    <form class="card" id="addToList" @submit.prevent="handleSubmit()">
         <h2>Add to the list</h2>
-        <input v-model="name" type="text" placeholder="Your name" required></input>
-        <input v-model="gift" type="text" placeholder="Add your gift name"></input>
+        <input class="form-control" v-model="name" type="text" placeholder="Your name" required></input>
+        <input class="form-control" v-model="gift" type="text" placeholder="Add your gift name"></input>
         <button id="addGift" class='btn' type="button" @click="addGift()">Add another gift</button>
         <div class="form-footer">
-        <label for="naughty">Naughty?</label>
-        <input v-model="naughty" type="checkbox" required name="naughty"></input>
+        <label for="naughty"><strong>Naughty?</strong></label>
+        <input v-model="naughty" type="checkbox" name="naughty"></input>
         <button id="submit" class='btn' type="submit">add</button>
         </div>
     </form>
+</section>
 </template>
 
 <style scoped>
@@ -36,10 +44,11 @@ const addGift = () => {
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin: 20px;
-    gap: 10px;
-    padding: 20px;
-    border: 1px solid #b30f0f;
+    margin: 1rem;
+    gap: 1.5rem;
+    padding: 3rem;
+    border: 2px solid #b30f0f;
+  
 }
 h2 {
     margin-bottom: 10px;
@@ -49,16 +58,22 @@ h2 {
     flex-direction: row;
     justify-content: space-between;
     width: 100%;
+    padding: 0 1rem;
 }
 label{
-    padding-top: 5px;
+    padding-top: 0.5rem;
 }
 .btn {
     padding: 5px 15px;
-    margin: 5px;
     background-color: #b30f0f;
+    opacity: 0.9;
     color: white;
     border: none;
     cursor: pointer;
 }
+.btn:hover {
+    scale: 1.1;
+}
+
+
 </style>
